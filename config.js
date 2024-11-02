bd4045aee3df7dfaa5e3f35a45ea8e8d05dfa8c9
ADong23-star <kuv0194@gmail.com>
2024-11-03 00:21:30 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = 'https://scontent.fhan15-2.fna.fbcdn.net/v/t39.30808-6/461796266_561234002998934_8144418748585242653_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHqIH-bsGkrzObE_w-vOI4iBh5p2fZWa9gGHmnZ9lZr2L4P8l7Rr5ryQIW7z2-KhjRssVni1RKT-xSJ2xI9zw5Z&_nc_ohc=tWlwA40E1dsQ7kNvgHby8l0&_nc_zt=23&_nc_ht=scontent.fhan15-2.fna&_nc_gid=Az-v3ZYDgQ-uAvOm4x-yRIA&oh=00_AYDK4Emg5_VoFE7eWjyh3gw8VN42RDnLocq6vdpwxc2hng&oe=672C0566';
+const giftImageBase64 = "data:image/png;base64, " + base64;
